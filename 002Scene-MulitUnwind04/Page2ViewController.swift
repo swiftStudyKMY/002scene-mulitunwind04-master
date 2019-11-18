697ba80131f521c93d8d242b95c8ada09844e0da
KMY <muziq43@hanafn.com>
2019-11-18 09:20:01 +0900

    Initial Commit

---
+++ 002Scene-MulitUnwind04/Page2ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Page2ViewController.swift
+//  002Scene-MulitUnwind04
+//
+//  Created by 김민영 on 11/5/19.
+//  Copyright © 2019 김민영. All rights reserved.
+//
+
+//import Foundation
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue){
+        print("gotoPage2 tapped");
+    }
+    
+}
---
+++ 002Scene-MulitUnwind04/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  002Scene-MulitUnwind04
+//
+//  Created by 김민영 on 11/5/19.
+//  Copyright © 2019 김민영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage1(_ segue : UIStoryboardSegue){
+        print("gotoPage1 tapped");
+    }
+
+
+}
+
